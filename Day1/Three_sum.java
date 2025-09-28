ac42d354c9f6f2b6ea5a4da238fac3f035b22f0d
Aadya Sharma <138554910+Aadya242005@users.noreply.github.com>
2025-09-28 21:58:50 +0530

    Add files via upload

---
+++ Day1/Three_sum.java
@@ -0,0 +1,54 @@
+package lec1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Three_sum {
+   public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = sol.threeSum(nums);
+
+        System.out.println(result); 
+       
+    }
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);  
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; 
+                } else {
+                    right--; 
+                }
+            }
+        }
+        return ans;
+    }
+
+    
+}
