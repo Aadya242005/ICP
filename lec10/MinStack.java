91110baefb73997378ed5281b3b19e7713c7c50d
Aadya Sharma <138554910+Aadya242005@users.noreply.github.com>
2025-10-28 15:20:57 +0530

    Add files via upload

---
+++ lec10/MinStack.java
@@ -0,0 +1,42 @@
+/*package Assignment_10.MInStack;
+
+import java.util.*;
+
+class MinStack {
+    private class Pair{
+        int val, min;
+        Pair(int v, int m){
+            this.val = v;
+            this.min = m;
+        }
+    }
+
+    private Stack<Pair> st;
+    private int min;
+
+    public MinStack() {
+        st = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int val) {
+        min = Math.min(min, val);
+        st.push(new Pair(val, min));
+    }
+    
+    public void pop() {
+        st.pop();
+        min =  st.isEmpty() ? Integer.MAX_VALUE : st.peek().min;
+    }
+    
+    public int top() {
+        return st.peek().val;
+    }
+    
+    public int getMin() {
+        return st.peek().min;
+    }
+}
+
+
+ */
